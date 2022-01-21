ade866ead3dc52ad62f0c2bf957831a7b3e3fe41
DublinSwords <victorcotov@gmail.com>
2022-01-21 11:23:26 +0000

    add fisrt question to javascript with 4 answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
      console.log('Started')
+     setNextQuestion()
 }
 function setNextQuestion() {
 
@@ -12,3 +14,15 @@ function setNextQuestion() {
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'What is the capital of Iceland?',
+        answers: [
+            { text: 'Reykjavík', correct: true },
+            { text: 'Stockholm', correct: false},
+            { text: 'Oslo', correct: false},
+            { text: 'Copenhagen', correct: false}
+        ]
+    }
+]
